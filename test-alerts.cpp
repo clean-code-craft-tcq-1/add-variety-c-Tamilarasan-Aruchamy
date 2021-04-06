6162c9d9ddd9917ee44a8ff01b8aa670bd9b43a4
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-04-06 20:04:39 +0530

    Adding testcase name

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,17 +5,17 @@
 
 extern int checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
-TEST_CASE("infers the breach according to PASSIVE_COOLING and send to CONTROLLER") {
+TEST_CASE("infers the breach(TOO_LOW) according to PASSIVE_COOLING and send to CONTROLLER") {
   BatteryCharacter batteryChar={PASSIVE_COOLING,"Bosch"};
   REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,-5) == ((TOO_LOW+1)*(TO_CONTROLLER+1)));
 }
 
-TEST_CASE("infers the breach according to HI_ACTIVE_COOLING and send to EMAIL") {
+TEST_CASE("infers the breach(TOO_HIGH) according to HI_ACTIVE_COOLING and send to EMAIL") {
   BatteryCharacter batteryChar={HI_ACTIVE_COOLING,"Bosch"};
   REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,50) == ((TOO_HIGH+1)*(TO_EMAIL+1)));
 }
 
-TEST_CASE("infers the breach according to MED_ACTIVE_COOLING and send to CONSOLE") {
+TEST_CASE("infers the breach(NORMAL) according to MED_ACTIVE_COOLING and send to CONSOLE") {
   BatteryCharacter batteryChar={MED_ACTIVE_COOLING,"Bosch"};
   REQUIRE(checkAndAlert(TO_CONSOLE,batteryChar,20) == ((NORMAL+1)*(TO_CONSOLE+1)));
 }
