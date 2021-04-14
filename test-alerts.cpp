17b4e74b82d3987b7809c8e9981a6ea854dce863
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-04-14 21:11:22 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -15,17 +15,55 @@ TEST_CASE("infers the breach(TOO_LOW) according to PASSIVE_COOLING and send to C
   
   TestInterface_Reset();
   BreachTypeActual=checkAndAlert(TO_CONTROLLER,batteryChar,-5);
-  
-  
-  
-  REQUIRE(BreachTypeActual == TOO_LOW);
-  
+   
+  REQUIRE(BreachTypeActual == TOO_LOW); 
   REQUIRE(Test_Controller_header == 0xfeed);
   REQUIRE(Test_Controller_breachType == TOO_LOW);
   REQUIRE(Test_Mail_recepient == NULL);
   REQUIRE(Test_Mail_MailContent == NULL);
-  REQUIRE(Test_Console_ConsoleContent == NULL);
+  REQUIRE(Test_Console_ConsoleContent == NULL);  
+  REQUIRE(Func_CallCount_Controller == 1);
+  REQUIRE(Func_CallCount_Mail == 0);
+  REQUIRE(Func_CallCount_printf == 0);  
   
+}
+
+
+TEST_CASE("infers the breach(NORMAL) according to PASSIVE_COOLING and send to CONTROLLER") {
+	
+  BreachType BreachTypeActual;
+  BatteryCharacter batteryChar={PASSIVE_COOLING,"Bosch"};
+  
+  TestInterface_Reset();
+  BreachTypeActual=checkAndAlert(TO_CONTROLLER,batteryChar,30);
+   
+  REQUIRE(BreachTypeActual == NORMAL); 
+  REQUIRE(Test_Controller_header == 0xfeed);
+  REQUIRE(Test_Controller_breachType == NORMAL);
+  REQUIRE(Test_Mail_recepient == NULL);
+  REQUIRE(Test_Mail_MailContent == NULL);
+  REQUIRE(Test_Console_ConsoleContent == NULL);  
+  REQUIRE(Func_CallCount_Controller == 1);
+  REQUIRE(Func_CallCount_Mail == 0);
+  REQUIRE(Func_CallCount_printf == 0);  
+  
+}
+
+
+TEST_CASE("infers the breach(TOO_HIGH) according to PASSIVE_COOLING and send to CONTROLLER") {
+	
+  BreachType BreachTypeActual;
+  BatteryCharacter batteryChar={PASSIVE_COOLING,"Bosch"};
+  
+  TestInterface_Reset();
+  BreachTypeActual=checkAndAlert(TO_CONTROLLER,batteryChar,40);
+   
+  REQUIRE(BreachTypeActual == TOO_HIGH); 
+  REQUIRE(Test_Controller_header == 0xfeed);
+  REQUIRE(Test_Controller_breachType == TOO_HIGH);
+  REQUIRE(Test_Mail_recepient == NULL);
+  REQUIRE(Test_Mail_MailContent == NULL);
+  REQUIRE(Test_Console_ConsoleContent == NULL);  
   REQUIRE(Func_CallCount_Controller == 1);
   REQUIRE(Func_CallCount_Mail == 0);
   REQUIRE(Func_CallCount_printf == 0);  
