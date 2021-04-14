ac6d7096684fe18af32f4c98f92b0490d456328a
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-04-14 20:01:47 +0530

    Add files via upload

---
+++ TestDoublesHeader.h
@@ -0,0 +1,7 @@
+extern int TestInterface_Reset();
+extern int Controller(const unsigned short header,BreachType breachType);
+extern int Mail(char* recepient,char* MailContent);
+extern int printf(char* ConsoleContent);
+extern char* ReadRecepient();
+extern char* ReadHeader();
+
