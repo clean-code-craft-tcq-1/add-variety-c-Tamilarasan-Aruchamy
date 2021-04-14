d213beeced891c82e3ac181fe3461b4f8f0600d8
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-04-14 20:03:09 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -2,26 +2,33 @@
 
 #include "test/catch.hpp"
 #include "typewise-alert.h"
+#include "TestDoublesHeader.h"
+#include <stddef.h>
 
 extern int checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
-int expectedResult(BreachType breachType,AlertTarget alertTarget)
-{
-	return ((breachType+1)*(alertTarget+1)); // Why "+1" with return value? Enum value start from index 0 for BreachType,AlertTarget.So +1 is needed to calculation.
-	
-}
 
 TEST_CASE("infers the breach(TOO_LOW) according to PASSIVE_COOLING and send to CONTROLLER") {
+	
+  BreachType BreachTypeActual;
   BatteryCharacter batteryChar={PASSIVE_COOLING,"Bosch"};
-  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,-5) == expectedResult(TOO_LOW,TO_CONTROLLER));
+  
+  TestInterface_Reset();
+  BreachTypeActual=checkAndAlert(TO_CONTROLLER,batteryChar,-5);
+  
+  
+  
+  REQUIRE(BreachTypeActual == TOO_LOW);
+  
+  REQUIRE(Test_Controller_header == 0xfeed);
+  REQUIRE(Test_Controller_breachType == TOO_LOW);
+  REQUIRE(Test_Mail_recepient == NULL);
+  REQUIRE(Test_Mail_MailContent == NULL);
+  REQUIRE(Test_Console_ConsoleContent == NULL);
+  
+  REQUIRE(Func_CallCount_Controller == 1);
+  REQUIRE(Func_CallCount_Mail == 0);
+  REQUIRE(Func_CallCount_printf == 0);  
+  
 }
 
-TEST_CASE("infers the breach(TOO_HIGH) according to HI_ACTIVE_COOLING and send to EMAIL") {
-  BatteryCharacter batteryChar={HI_ACTIVE_COOLING,"Bosch"};
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,50) == expectedResult(TOO_HIGH,TO_EMAIL));
-}
-
-TEST_CASE("infers the breach(NORMAL) according to MED_ACTIVE_COOLING and send to CONSOLE") {
-  BatteryCharacter batteryChar={MED_ACTIVE_COOLING,"Bosch"};
-  REQUIRE(checkAndAlert(TO_CONSOLE,batteryChar,20) == expectedResult(NORMAL,TO_CONSOLE));
-}
