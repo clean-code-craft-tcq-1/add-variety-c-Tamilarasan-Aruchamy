f85367488939de1212e8372b60c5d6a578279e84
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-04-14 20:16:48 +0530

    Update TestDoublesHeader.h

--- TestDoublesHeader.h
+++ TestDoublesHeader.h
@@ -1,10 +1,10 @@
 
 extern unsigned short Test_Controller_header;
-extern BreachType Test_Controller_breachType;
+int BreachType Test_Controller_breachType;
 extern int Func_CallCount_Controller;
 
 extern char* Test_Mail_recepient;
-extern ConsoleMgsConfig* Test_Mail_MailContent;
+extern char* Test_Mail_MailContent;
 extern int Func_CallCount_Mail;
 
 extern char* Test_Console_ConsoleContent;
@@ -17,5 +17,5 @@ extern int Controller(const unsigned short header,BreachType breachType);
 extern int Mail(char* recepient,char* MailContent);
 extern int printf(char* ConsoleContent);
 extern char* ReadRecepient();
-extern char* ReadHeader();
+extern unsigned short ReadHeader();
 
